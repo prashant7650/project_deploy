d577836c19bbb7ccb81b9b6c2f635644560ffe64
prashant7650 <prashantkad240999@gmail.com>
2023-02-25 21:10:37 +0530

    c

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,19 +1,25 @@
 const express = require("express");
-const {productModel} = require("../model/product.model")
+const { productModel } = require("../model/product.model")
 
 const productRouter = express.Router()
 
-productRouter.get("/", async (req,res) => {
-
+productRouter.get("/", async (req, res) => {
+try{
     const notes = await productModel.find()
-    res.status(200).json({notes})
-})
-productRouter.post("/create", async (req,res) => {
-    const payload = req.body
-    const note  = new productModel(payload)
-     await note.save()
-     res.send("product Created")
+    res.status(200).json({ notes })
+}
+ catch (error) {
+    console.log(error)
+    res.send(error)
+}
    
+})
+productRouter.post("/create", async (req, res) => {
+    const payload = req.body
+    const note = new productModel(payload)
+    await note.save()
+    res.send("product Created")
+
 })
 // productRouter.patch("/update/:id", async (req,res) => {
 //     const noteID = req.params.id
@@ -26,6 +32,6 @@ productRouter.post("/create", async (req,res) => {
 //     await productModel.findByIdAndDelete({_id:noteID})
 //     res.send({"msg":`Note with id  ${noteID} has been delted`})
 // })
-module.exports ={
+module.exports = {
     productRouter
 }
